a428c6593c64abb822ca29d779df4a9910a9cebc
glori0us1407 <108141513+glori0us1407@users.noreply.github.com>
2022-06-27 12:09:36 +0900

    Add files via upload

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports ={
+    mongoURI: 'mongodb+srv://glori0us:nb1221517@boilerplate.xe4hhj8.mongodb.net/?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -2,11 +2,28 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://glori0us:nb1221517@boilerplate.xe4hhj8.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
 .then(() => console.log('MongoDB Connected!'))
     .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World'))
+app.get('/', (req, res) => res.send('Hello World2'))
+
+app.post('/register', (req, res) =>{
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log('Example app listening on port ${port}!'))
